0082fc3d1c87b6b90c0ff3cfbda08afbaab7d036
Kazuhiro Mitsukuni <mitsu.kz.cog@gmail.com>
2019-08-18 09:23:47 +0000

    I prepared

--- src/main/java/com/rakuten/internship/entity/Todo.java
+++ src/main/java/com/rakuten/internship/entity/Todo.java
@@ -1,9 +1,15 @@
 package com.rakuten.internship.entity;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 /**
  * このクラスは、タスク管理の対象となるTodoのエンティティーです。
  * エンティティーとして使えるように、コードを記入してください。
  */
+
 public class Todo {
     //push
     
